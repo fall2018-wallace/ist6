629dca1f315701d88c19daa27b7e7682e5b99108
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 13:39:01 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -6,3 +6,4 @@ arrests<- cbind(rownames(arrests),arrests)
 colnames(arrests)= c("StateName")
 arrests
 merge_data <- merge(arrests,MergeCensus)
+colnames(merge_data) <- c("StateName","Murder","Assault","UrbanPop","Rape","Population","popOver18","PercentOver18")
