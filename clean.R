f68021d1cdd7b036f2342d74c083f935f6f9adaf
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 12:56:59 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -6,7 +6,8 @@ readStates <-function(States)
     States <- States[-1]
     num.row <- nrow(States)
     States <- States[-num.row]
-    
-    
+    States <- States[,-1:4]
+    colnames(States) <- c("StateName","Population", "PopOver18","PercentOver18")
+    return(States)
     
 }
