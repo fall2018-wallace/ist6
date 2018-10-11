4813337d8dae10bd5c47465c2285aceb7b205168
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:32:21 -0400

    Saving Histogram.R

--- Histogram.R
+++ Histogram.R
@@ -2,3 +2,13 @@
 
 library(ggplot2)
 options(scipen=999)
+
+ggplot(mergedDataframe,aes(x=population))+geom_histogram(bins = 60, color = "red", fill = "red")+ggtitle("Population Histogram")
+
+ggplot(mergedDataframe,aes(x=Murder))+geom_histogram(bins = 60, color = "blue", fill = "blue")+ggtitle("Murder Histogram")
+
+ggplot(mergedDataframe,aes(x=Assault))+geom_histogram(bins = 60, color = "green", fill = "green")+ggtitle("Assault Histogram")
+
+ggplot(mergedDataframe,aes(x=UrbanPop))+geom_histogram(bins = 60, color = "yellow", fill = "yellow")+ggtitle("Urban Population Histogram")
+
+ggplot(mergedDataframe,aes(x=Rape))+geom_histogram(bins = 60, color = "pink", fill = "pink")+ggtitle("Rape Histogram")
