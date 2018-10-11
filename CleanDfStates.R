7165972ee77b7e9e786a9bc55beb2839a6940709
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:12:06 -0400

    Saving CleanDfStates.R

--- CleanDfStates.R
+++ CleanDfStates.R
@@ -2,7 +2,7 @@
 
 dfStates1 <- dfStates[ , -1:-4] #We use this format to delete the first 4 columns in the dataset which we have.
 colnames(dfStates1)[colnames(dfStates1)=="NAME"] <- "stateName" 
-View(dfStates1)  #We use the colnames() function below to change the names of the columns.
+dfStates1 #We use the colnames() function below to change the names of the columns.
 
 NewFunc <- function()
 {
@@ -10,4 +10,4 @@ NewFunc <- function()
 }
 
 NewFunc()
-View(dfStates1)
+dfStates1
