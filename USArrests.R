773e3a7a665fa998e0c41924e6fdc6df9ceabda8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:20:42 -0400

    Saving USArrests.R

--- USArrests.R
+++ USArrests.R
@@ -1,6 +1,6 @@
 
 ArrestData <- USArrests #Copied the USArrests data set into a local variable
 ArrestData
-
+ArrestData <- cbind(rownames(ArrestData),ArrestData)
 colnames(ArrestData) <- c("stateName") 
 ArrestData
