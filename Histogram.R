8e6703397e8b84573aed023e7b6b6c46b533c9c3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:33:55 -0400

    Saving Histogram.R

--- Histogram.R
+++ Histogram.R
@@ -3,12 +3,12 @@
 library(ggplot2)
 options(scipen=999)
 
-ggplot(mergedDataframe,aes(x=population))+geom_histogram(bins = 60, color = "red", fill = "red")+ggtitle("Population Histogram")
+a <- ggplot(mergedDataframe,aes(x=population))+geom_histogram(bins = 60, color = "red", fill = "red")+ggtitle("Population Histogram")
 
-ggplot(mergedDataframe,aes(x=Murder))+geom_histogram(bins = 60, color = "blue", fill = "blue")+ggtitle("Murder Histogram")
+b <- ggplot(mergedDataframe,aes(x=Murder))+geom_histogram(bins = 60, color = "blue", fill = "blue")+ggtitle("Murder Histogram")
 
-ggplot(mergedDataframe,aes(x=Assault))+geom_histogram(bins = 60, color = "green", fill = "green")+ggtitle("Assault Histogram")
+c <-ggplot(mergedDataframe,aes(x=Assault))+geom_histogram(bins = 60, color = "green", fill = "green")+ggtitle("Assault Histogram")
 
-ggplot(mergedDataframe,aes(x=UrbanPop))+geom_histogram(bins = 60, color = "yellow", fill = "yellow")+ggtitle("Urban Population Histogram")
+d <- ggplot(mergedDataframe,aes(x=UrbanPop))+geom_histogram(bins = 60, color = "yellow", fill = "yellow")+ggtitle("Urban Population Histogram")
 
-ggplot(mergedDataframe,aes(x=Rape))+geom_histogram(bins = 60, color = "pink", fill = "pink")+ggtitle("Rape Histogram")
+e <- ggplot(mergedDataframe,aes(x=Rape))+geom_histogram(bins = 60, color = "pink", fill = "pink")+ggtitle("Rape Histogram")
