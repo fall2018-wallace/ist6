72773af9ba5a7facfd09eb0e5f1fe234e502fe1c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:26:27 -0400

    Saving MergeData.R

--- MergeData.R
+++ MergeData.R
@@ -1,5 +1,5 @@
 
-mergedDataframe <- merge(dfStates1,ArrestData,by = "stateName")
+mergedDataframe <- merge(dfStates1,ArrestData)
 View(mergedDataframe)
 colnames(mergedDataframe)=c("stateName","population","popOver18","percentOver18","Murder","Assault","UrbanPop","Rape")
 mergedDataframe
