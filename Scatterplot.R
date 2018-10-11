e10351569a0a3925e0720ee3d9169913f98b3822
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:57:18 -0400

    Saving Scatterplot.R

--- Scatterplot.R
+++ Scatterplot.R
@@ -2,4 +2,5 @@
 
 library(ggplot2)
 options(scipen=999)
-ggplot(mergedDataframe,aes(x=population,y=percentOver18)) + geom_point(aes(size=Murder,color=Murder))
+
+k <- ggplot(mergedDataframe,aes(x=population,y=percentOver18)) + geom_point(aes(size=Murder,color=Murder))
