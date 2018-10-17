d0f9edae81ee97b1980ad2a0c5a7ad2941f83fa0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 01:05:56 -0400

    Saving MapColouring.R

--- MapColouring.R
+++ MapColouring.R
@@ -1,1 +1,9 @@
 
+mergedDataframe1$stateName= tolower(mergedDataframe1$stateName) #ggplot wants all the statenames to be in lower case thus we used the tolower command.
+
+us<-map_data("state") #We get the map of USA with the following line of code
+map.simple<- ggplot(mergedDataframe1, aes(map_id=stateName)) 
+map.simple<- map.simple + geom_map(map=us, fill="white",color="black") #With geom_map() function, We tell ggplot the map to use and simply fill each state white with black outlines
+map.simple<- map.simple +expand_limits(x= us$long, y= us$lat) #We expand the limits based on longitudes and latitudes for the united states
+map.simple
+map.simple<-map.simple +coord_map()+ ggtitle("Map of USA") #We use this function to keep the map from being distorted or stretched
