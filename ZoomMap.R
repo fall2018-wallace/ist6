926d35fba4dc633208a0ca539627c8cd456ef2ea
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 01:56:08 -0400

    Saving ZoomMap.R

--- ZoomMap.R
+++ ZoomMap.R
@@ -1,4 +1,13 @@
 
+# 6)   Repeat step C, but only show the states in the north east
+
+# Hint: get the lat and lon of new york city
+
+# Hint: set the xlim and ylim to NYC +/- 10
 library(maps)
 library(ggplot2)
 options(scipen=999)
+
+latlon <- geocode(source = "dsk","nyc, new york, ny")
+
+latlon
