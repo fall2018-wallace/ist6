777fae47e90ebf176bcdb61f61492e42311dab23
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 01:02:19 -0400

    Saving AreaCentre.R

--- AreaCentre.R
+++ AreaCentre.R
@@ -7,5 +7,5 @@ area<- state.area #Reading in the inbuilt dataset state.area
 center<- state.center #Reading in the inbuilt datadrame state.center
 dataframe<- data.frame(stateName,area,center) #Creates a dataframe of the three variables 
 dataframe 
-mergedDF<- merge(mergedDataframe, dataframe, by='stateName') #Merges the dataframes MergedDataframes and Dataframe
-mergedDF
+mergedDataFrame1<- merge(mergedDataframe, dataframe, by='stateName') #Merges the dataframes MergedDataframes and Dataframe
+mergedDataFrame1
