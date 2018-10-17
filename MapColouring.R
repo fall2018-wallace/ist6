4fb2c9f7aac70301efe80b6fa0d6a0e2f621fb3e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 01:17:43 -0400

    Saving MapColouring.R

--- MapColouring.R
+++ MapColouring.R
@@ -8,5 +8,5 @@ us<-map_data("state") #We get the map of USA with the following line of code
 map.simple<- ggplot(mergedDataFrame1, aes(map_id=stateName)) 
 map.simple<- map.simple + geom_map(map=us, fill="white",color="black") #With geom_map() function, We tell ggplot the map to use and simply fill each state white with black outlines
 map.simple<- map.simple +expand_limits(x= us$long, y= us$lat) #We expand the limits based on longitudes and latitudes for the united states
-map.simple
+map <- map.simple
 map.simple<-map.simple +coord_map()+ ggtitle("Map of USA") #We use this function to keep the map from being distorted or stretched
