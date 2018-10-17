1bdebabc6485e95e75713a9da184312715ad0298
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 00:59:13 -0400

    Saving AreaCentre.R

--- AreaCentre.R
+++ AreaCentre.R
@@ -1,3 +1,11 @@
 
 library(maps)
 library(ggplot2)
+
+stateName<- state.name #Reading in the inbuild dataset state.name
+area<- state.area #Reading in the inbuilt dataset state.area
+center<- state.center #Reading in the inbuilt datadrame state.center
+dataframe<- data.frame(stateName,area,center) #Creates a dataframe of the three variables 
+View(dataframe) 
+mergedDF<- merge(mergedDataframe1, dataframe, by='stateName') #Merges the dataframes MergedDataframes and Dataframe
+View(mergedDF)
