aa1fb46077b0018311d9180ae0592072d16ac767
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 20:42:53 -0400

    Saving MapColouring.R

--- MapColouring.R
+++ MapColouring.R
@@ -30,3 +30,26 @@ map.point<- map.point +expand_limits(x= us$long, y= us$lat)
 map.point<- map.point + geom_point(aes(x = x, y = y, size=population))
 map.point<-map.point +coord_map()+ ggtitle("Population per state")
 population <- map.point
+
+
+
+# 6)   Repeat step C, but only show the states in the north east
+
+# Hint: get the lat and lon of new york city
+
+# Hint: set the xlim and ylim to NYC +/- 10
+library(maps)
+library(ggplot2)
+library(ggmap)
+options(scipen=999)
+
+latlon <- geocode(source = "dsk","nyc, new york, ny")
+
+latlon
+
+mapZoom <- mapZoom + xlim(latlon$lon-10,latlon$lon+10) + ylim(latlon$lat-10, latlon$lat+10)
+
+mapZoom <- mapZoom + coord_map() 
+
+
+zoomedmap <-mapZoom
